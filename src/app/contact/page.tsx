79d981a1b0f80ec6e5a32de4f63b9ef8b1835628
Oliver Ip <oliveripkanam@gmail.com>
2025-05-07 15:52:58 +0100

    fix: Resolve build errors

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -129,7 +129,7 @@ export default function ContactPage() {
           <input type="hidden" name="form-name" value="contact" />
           <p className="hidden">
             <label>
-              Don't fill this out if you're human: <input name="bot-field" />
+              Don&apos;t fill this out if you&apos;re human: <input name="bot-field" />
             </label>
           </p>
 
